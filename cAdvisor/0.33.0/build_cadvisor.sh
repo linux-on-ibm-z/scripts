a5ac10c9be026e57c15ee4c721c24b7cf9aa721a
aborkar-ibm <aborkar@us.ibm.com>
2019-03-26 14:15:42 +0530

    Updated Comment

--- cAdvisor/0.33.0/build_cadvisor.sh
+++ cAdvisor/0.33.0/build_cadvisor.sh
@@ -3,7 +3,7 @@
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
-# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/cAdvisor/build_cadvisor.sh
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/cAdvisor/0.33.0/build_cadvisor.sh
 # Execute build script: bash build_cadvisor.sh    (provide -h for help)
 #
 
