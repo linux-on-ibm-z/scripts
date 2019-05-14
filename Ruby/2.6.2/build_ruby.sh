af98758fe42c5453cbfcfaac4b48ba67c8a5d2e4
aborkar-ibm <aborkar@us.ibm.com>
2019-05-14 14:50:26 +0530

    Added Ubuntu 19.04

--- Ruby/2.6.2/build_ruby.sh
+++ Ruby/2.6.2/build_ruby.sh
@@ -153,7 +153,7 @@ checkPrequisites  #Check Prequisites
 
 DISTRO="$ID-$VERSION_ID"
 case "$DISTRO" in
-"ubuntu-16.04" | "ubuntu-18.04")
+"ubuntu-16.04" | "ubuntu-18.04" | "ubuntu-19.04" )
   printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
   sudo apt-get update > /dev/null
   sudo apt-get install -y  gcc make wget tar bzip2 subversion bison flex openssl |& tee -a "${LOG_FILE}"
