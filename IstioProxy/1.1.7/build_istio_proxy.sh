421363c3e780a417485fcee11c8da3294b6439e3
aborkar-ibm <aborkar@us.ibm.com>
2019-06-28 18:29:42 +0530

    Added Ubuntu 19.04

--- IstioProxy/1.1.7/build_istio_proxy.sh
+++ IstioProxy/1.1.7/build_istio_proxy.sh
@@ -11,6 +11,7 @@ set -e -o pipefail
 
 PACKAGE_NAME="Istio Proxy"
 PACKAGE_VERSION="1.1.7"
+GO_VERSION="1.10.5"
 CURDIR="$(pwd)"
 REPO_URL="https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/IstioProxy/1.1.7/patch"
 ISTIO_PROXY_REPO_URL="https://github.com/istio/proxy.git"
@@ -27,7 +28,6 @@ if [ ! -d "$CURDIR/logs/" ]; then
 	mkdir -p "$CURDIR/logs/"
 fi
 
-#
 if [ -f "/etc/os-release" ]; then
 	source "/etc/os-release"
 fi
@@ -69,19 +69,21 @@ function prepare() {
 
 function runTest() {
 	set +e
-        if [[ "$TESTS" == "true" ]]; then
-                printf -- 'Running tests \n\n' |& tee -a "$LOG_FILE"    
-	        if [ "${VERSION_ID}" == "12.4" ]; then
-		    cd "${CURDIR}"
-		    curl -o Makefile_test_sl12.4.diff $REPO_URL/Makefile_test_sl12.4.diff
-		    patch "${CURDIR}/proxy/Makefile" Makefile_test_sl12.4.diff
+	if [[ "$TESTS" == "true" ]]; then
+		printf -- 'Running tests \n\n' |& tee -a "$LOG_FILE"
+		if [ "${VERSION_ID}" == "12.4" ]; then
+			cd "${CURDIR}"
+			curl -o Makefile_test_sl12.4.diff $REPO_URL/Makefile_test_sl12.4.diff
+			patch "${CURDIR}/proxy/Makefile" Makefile_test_sl12.4.diff
 		else
-		    cd "${CURDIR}"
-		    curl -o Makefile_test.diff $REPO_URL/Makefile_test.diff		
-		    patch "${CURDIR}/proxy/Makefile" Makefile_test.diff	
+			cd "${CURDIR}"
+			curl -o Makefile_test.diff $REPO_URL/Makefile_test.diff
+			patch "${CURDIR}/proxy/Makefile" Makefile_test.diff
 		fi
 		cd "${CURDIR}/proxy"
 		make test
+		printf -- '\nNote: One test case ( `//test/integration:mixer_fault_test` ) is failing on both x86 and s390x platform \n' |& tee -a "$LOG_FILE"
+		printf -- 'Kindly ignore this failure and proceed with further instructions.\n' |& tee -a "$LOG_FILE"
 	fi
 	set -e
 }
@@ -89,7 +91,6 @@ function runTest() {
 function cleanup() {
 	printf -- '\nCleaned up the artifacts\n' |& tee -a "$LOG_FILE"
 	rm -rf "${CURDIR}/cmake-3.7.2.tar.gz"
-	rm -rf "${CURDIR}/ninja"
 	rm -rf "${CURDIR}/go1.10.5.linux-s390x.tar.gz"
 	rm -rf "${CURDIR}/BUILD.diff"
 	rm -rf "${CURDIR}/WORKSPACE.diff"
@@ -107,12 +108,12 @@ function cleanup() {
 	rm -rf "${CURDIR}/BUILD-exe.patch"
 	rm -rf "${CURDIR}/BUILD-api.patch"
 	rm -rf "${CURDIR}/gcc-7.3.0.tar.xz"
-	
+
 }
 
 function buildGCC() {
 
-	printf -- 'Building GCC \n'
+	printf -- 'Building GCC \n' |& tee -a "$LOG_FILE"
 	cd "${CURDIR}"
 	wget https://ftpmirror.gnu.org/gcc/gcc-7.3.0/gcc-7.3.0.tar.xz
 	tar -xf gcc-7.3.0.tar.xz
@@ -121,8 +122,8 @@ function buildGCC() {
 	mkdir gcc_build
 	cd gcc_build/
 	../configure --prefix=/opt/gcc --enable-languages=c,c++ --with-arch=zEC12 --with-long-double-128 \
-             --build=s390x-linux-gnu --host=s390x-linux-gnu --target=s390x-linux-gnu                  \
-             --enable-threads=posix --with-system-zlib --disable-multilib
+		--build=s390x-linux-gnu --host=s390x-linux-gnu --target=s390x-linux-gnu \
+		--enable-threads=posix --with-system-zlib --disable-multilib
 	make -j 8
 	sudo make install
 	sudo ln -sf /opt/gcc/bin/gcc /usr/bin/gcc
@@ -132,7 +133,6 @@ function buildGCC() {
 	export LD_LIBRARY_PATH=/opt/gcc/lib64:"$LD_LIBRARY_PATH"
 	export C_INCLUDE_PATH=/opt/gcc/lib/gcc/s390x-linux-gnu/7.3.0/include
 	export CPLUS_INCLUDE_PATH=/opt/gcc/lib/gcc/s390x-linux-gnu/7.3.0/include
-	
 
 	#for rhel
 	if [[ "${ID}" == "rhel" ]]; then
@@ -147,17 +147,17 @@ function buildGCC() {
 function buildGO() {
 	cd "${CURDIR}"
 	if command -p "go" version | grep 1.10 >/dev/null; then
-			printf -- "Go detected\n"
+		printf -- "Go detected\n"
 	else
-			printf -- 'Installing go\n'
-			cd "${CURDIR}"
-			wget https://storage.googleapis.com/golang/go1.10.5.linux-s390x.tar.gz
-			tar -xzf go1.10.5.linux-s390x.tar.gz
-			export PATH=${CURDIR}/go/bin:$PATH
-			export GOROOT=${CURDIR}/go
-			go version
-			printf -- 'go installed\n'
-	fi	
+		printf -- 'Installing go\n'
+		cd "${CURDIR}"
+		wget "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-s390x.tar.gz"
+		tar -xzf "go${GO_VERSION}.linux-s390x.tar.gz"
+		export PATH=${CURDIR}/go/bin:$PATH
+		export GOROOT=${CURDIR}/go
+		go version
+		printf -- 'go installed\n'
+	fi
 }
 function installDependency() {
 	printf -- 'Installing dependencies\n' |& tee -a "$LOG_FILE"
@@ -172,18 +172,21 @@ function installDependency() {
 	fi
 	cd "${CURDIR}"
 
-			printf -- 'Installing Java\n'
-			cd "${CURDIR}"
-			wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jdk_s390x_linux_hotspot_11.0.2_9.tar.gz
-			tar -xvf OpenJDK11U-jdk_s390x_linux_hotspot_11.0.2_9.tar.gz
-			export JAVA_HOME=${CURDIR}/jdk-11.0.2+9
-			export PATH=$JAVA_HOME/bin:$PATH
-			java -version
-			printf -- 'java installed\n'
-	
+	printf -- 'Installing Java\n' |& tee -a "$LOG_FILE"
+	cd "${CURDIR}"
+	wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jdk_s390x_linux_hotspot_11.0.2_9.tar.gz
+	tar -xvf OpenJDK11U-jdk_s390x_linux_hotspot_11.0.2_9.tar.gz
+	export JAVA_HOME=${CURDIR}/jdk-11.0.2+9
+	export PATH=$JAVA_HOME/bin:$PATH
+	java -version |& tee -a "$LOG_FILE"
+	printf -- 'java installed\n' |& tee -a "$LOG_FILE"
+	#export CC even if bazel is pre-installed
+	export CC=/usr/bin/gcc
+	export CXX=/usr/bin/g++
+
 	printf -- 'Downloading Bazel\n' |& tee -a "$LOG_FILE"
-	if command -p "bazel" version | grep 0.24.1 >/dev/null; then
-			printf -- 'Bazel detected\n' |& tee -a "$LOG_FILE"
+	if command "bazel" version | grep 0.24.1 >/dev/null; then
+		printf -- 'Bazel detected\n' |& tee -a "$LOG_FILE"
 	else
 
 		#Bazel download
@@ -194,7 +197,7 @@ function installDependency() {
 		chmod -R +w .
 		export CC=/usr/bin/gcc
 		export CXX=/usr/bin/g++
-		
+
 		cd "${CURDIR}"
 		curl -o BUILD.diff $REPO_URL/BUILD.diff
 		patch "${CURDIR}/bazel/tools/cpp/BUILD" BUILD.diff
@@ -206,25 +209,25 @@ function installDependency() {
 		patch "${CURDIR}/bazel/scripts/bootstrap/compile.sh" compile.sh.diff
 		cd ${CURDIR}/bazel
 		env EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk" bash ./compile.sh
-		export PATH=$PATH:${CURDIR}/bazel/output/
-		bazel version
+		export PATH=${CURDIR}/bazel/output/:$PATH
+		bazel version |& tee -a "$LOG_FILE"
 		printf -- 'Bazel installed\n' |& tee -a "$LOG_FILE"
 	fi
-	
-       if [ "${ID}" == "rhel" ] || [ ${VERSION_ID} == 12.4 ]; then
-                printf -- '\nDownloading ninja\n'
-                cd "${CURDIR}"
-                git clone -b v1.8.2 git://github.com/ninja-build/ninja.git && cd ninja
-                ./configure.py --bootstrap
-                if [ "${ID}" == "rhel" ]; then
-                sudo ln -sf ${CURDIR}/ninja/ninja /usr/local/bin/ninja
-                export PATH=/usr/local/bin:$PATH
-                else
-                sudo ln -sf ${CURDIR}/ninja/ninja /usr/bin/ninja
-                fi
-                ninja --version
-                printf -- '\nninja installed succesfully\n'
-        fi
+
+	if [ "${ID}" == "rhel" ] || [ ${VERSION_ID} == 12.4 ]; then
+		printf -- '\nDownloading ninja\n' |& tee -a "$LOG_FILE"
+		cd "${CURDIR}"
+		git clone -b v1.8.2 git://github.com/ninja-build/ninja.git && cd ninja
+		./configure.py --bootstrap
+		if [ "${ID}" == "rhel" ]; then
+			sudo ln -sf ${CURDIR}/ninja/ninja /usr/local/bin/ninja
+			export PATH=/usr/local/bin:$PATH
+		else
+			sudo ln -sf ${CURDIR}/ninja/ninja /usr/bin/ninja
+		fi
+		ninja --version |& tee -a "$LOG_FILE"
+		printf -- '\nninja installed succesfully\n' |& tee -a "$LOG_FILE"
+	fi
 }
 
 function configureAndInstall() {
@@ -238,7 +241,7 @@ function configureAndInstall() {
 	printf -- '\nDownloading Envoy\n'
 	git clone https://github.com/envoyproxy/envoy
 	cd envoy/
-	git checkout ac7aa5a   
+	git checkout ac7aa5a
 
 	#multiple patches to be user here
 	cd "${CURDIR}"
@@ -249,20 +252,20 @@ function configureAndInstall() {
 	curl -o luajit-patch.patch $REPO_URL/luajit-patch.patch
 	patch "${CURDIR}/envoy/bazel/foreign_cc/luajit.patch" luajit-patch.patch
 
-        curl -o repositories-envoy.bzl.patch $REPO_URL/repositories-envoy.bzl.patch
-        patch "${CURDIR}/envoy/bazel/repositories.bzl" repositories-envoy.bzl.patch
+	curl -o repositories-envoy.bzl.patch $REPO_URL/repositories-envoy.bzl.patch
+	patch "${CURDIR}/envoy/bazel/repositories.bzl" repositories-envoy.bzl.patch
 
-        curl -o BUILD-api.patch $REPO_URL/BUILD-api.patch
-        patch "${CURDIR}/envoy/source/common/api/BUILD" BUILD-api.patch
+	curl -o BUILD-api.patch $REPO_URL/BUILD-api.patch
+	patch "${CURDIR}/envoy/source/common/api/BUILD" BUILD-api.patch
 
-        curl -o BUILD-exe.patch $REPO_URL/BUILD-exe.patch
-        patch "${CURDIR}/envoy/source/exe/BUILD" BUILD-exe.patch
+	curl -o BUILD-exe.patch $REPO_URL/BUILD-exe.patch
+	patch "${CURDIR}/envoy/source/exe/BUILD" BUILD-exe.patch
 
 	curl -o repository_locations.bzl.patch $REPO_URL/repository_locations.bzl.patch
-        patch "${CURDIR}/envoy/bazel/repository_locations.bzl" repository_locations.bzl.patch
+	patch "${CURDIR}/envoy/bazel/repository_locations.bzl" repository_locations.bzl.patch
 
-        curl -o patch_utility.diff $REPO_URL/patch_utility.diff
-        patch "${CURDIR}/envoy/source/common/network/utility.cc" patch_utility.diff
+	curl -o patch_utility.diff $REPO_URL/patch_utility.diff
+	patch "${CURDIR}/envoy/source/common/network/utility.cc" patch_utility.diff
 
 	printf -- 'Envoy installed\n'
 
@@ -270,11 +273,11 @@ function configureAndInstall() {
 	cd "${CURDIR}"
 	printf -- '\nDownloading BoringSSL\n'
 	git clone https://github.com/linux-on-ibm-z/boringssl
-	cd boringssl  
+	cd boringssl
 	git checkout boringssl-Istio102-s390x
 	printf -- 'BoringSSL installed\n'
 	printenv >>"$LOG_FILE"
-	
+
 	#rules_foreign_cc download
 	cd "${CURDIR}"
 	printf -- '\nDownloading rules_foreign_cc\n'
@@ -284,7 +287,7 @@ function configureAndInstall() {
 	bazel build
 	printf -- 'rules_foreign_cc installed\n'
 	printenv >>"$LOG_FILE"
-	
+
 	cd "${CURDIR}"
 
 	# Download and configure  Istio Proxy
@@ -299,24 +302,21 @@ function configureAndInstall() {
 	cat WORKSPACE.diff
 	patch "${CURDIR}/proxy/WORKSPACE" WORKSPACE.diff
 
-	
-	
-	if [ -f "$PROXY_DEBUG_BIN_PATH/envoy" ]
-	then
-        printf -- "Istio Proxy binaries (Debug mode) are found at location $PROXY_DEBUG_BIN_PATH \n"
+	if [ -f "$PROXY_DEBUG_BIN_PATH/envoy" ]; then
+		printf -- "Istio Proxy binaries (Debug mode) are found at location $PROXY_DEBUG_BIN_PATH \n"
 	else
 		#Build Istio Proxy In DEBUG mode
 		printf -- '\nBuilding Istio Proxy In DEBUG mode\n'
 		printf -- '\nBuild might take some time.Sit back and relax\n'
-                if [ "${VERSION_ID}" == "12.4" ]; then
-		    cd "${CURDIR}"
-		    curl -o Makefile_debug_sl12.4.diff $REPO_URL/Makefile_debug_sl12.4.diff
-		    patch "${CURDIR}/proxy/Makefile" Makefile_debug_sl12.4.diff
+		if [ "${VERSION_ID}" == "12.4" ]; then
+			cd "${CURDIR}"
+			curl -o Makefile_debug_sl12.4.diff $REPO_URL/Makefile_debug_sl12.4.diff
+			patch "${CURDIR}/proxy/Makefile" Makefile_debug_sl12.4.diff
 		else
-		#Patch applied for debug mode
-		    cd "${CURDIR}"
-		    curl -o Makefile_debug.diff $REPO_URL/Makefile_debug.diff
-		    patch "${CURDIR}/proxy/Makefile" Makefile_debug.diff
+			#Patch applied for debug mode
+			cd "${CURDIR}"
+			curl -o Makefile_debug.diff $REPO_URL/Makefile_debug.diff
+			patch "${CURDIR}/proxy/Makefile" Makefile_debug.diff
 		fi
 		cd "${CURDIR}/proxy"
 		make build
@@ -327,8 +327,7 @@ function configureAndInstall() {
 
 	#Build Istio Proxy In RELEASE mode
 	cd "${CURDIR}"
-	if [ -f "$PROXY_RELEASE_BIN_PATH/envoy" ]
-	then
+	if [ -f "$PROXY_RELEASE_BIN_PATH/envoy" ]; then
 		printf -- "Istio Proxy binaries (Release mode) are found at location $PROXY_RELEASE_BIN_PATH \n"
 	else
 		printf -- '\nBuilding Istio Proxy In RELEASE mode\n'
@@ -341,7 +340,7 @@ function configureAndInstall() {
 			curl -o Makefile_release_ub18.diff $REPO_URL/Makefile_release_ub18.diff
 			patch "${CURDIR}/proxy/Makefile" Makefile_release_ub18.diff
 		elif [ "${VERSION_ID}" == "12.4" ]; then
-		        curl -o Makefile_release_sl12.4.diff $REPO_URL/Makefile_release_sl12.4.diff
+			curl -o Makefile_release_sl12.4.diff $REPO_URL/Makefile_release_sl12.4.diff
 			patch "${CURDIR}/proxy/Makefile" Makefile_release_sl12.4.diff
 		fi
 
@@ -352,7 +351,7 @@ function configureAndInstall() {
 		cp -r "${CURDIR}/proxy/bazel-bin/src/envoy/envoy" "${PROXY_RELEASE_BIN_PATH}/"
 		printf -- 'Built Istio Proxy successfully in RELEASE mode\n\n'
 	fi
-	
+
 	#Run tests
 	runTest
 
@@ -411,7 +410,7 @@ case "$DISTRO" in
 	sudo apt-get update
 	sudo apt-get install -y git tar pkg-config zip g++ zlib1g-dev unzip python libtool automake cmake curl wget build-essential realpath ninja-build clang-format-5.0 golang-1.10
 	export PATH=/usr/lib/go-1.10/bin:$PATH
-	buildGCC |& tee -a "$LOG_FILE"
+	buildGCC
 	installDependency
 	configureAndInstall |& tee -a "$LOG_FILE"
 
@@ -431,14 +430,29 @@ case "$DISTRO" in
 	configureAndInstall |& tee -a "$LOG_FILE"
 
 	;;
-	
-"rhel-7.4" | "rhel-7.5" | "rhel-7.6" )
+
+"ubuntu-19.04")
+	printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+	printf -- '\nInstalling dependencies \n' |& tee -a "$LOG_FILE"
+	sudo apt-get update
+	sudo apt-get install -y git pkg-config zip zlib1g-dev unzip python libtool automake cmake curl wget build-essential rsync clang gcc-7 g++-7 libgtk2.0-0 ninja-build clang-format-8 golang-1.10
+	sudo rm -rf /usr/bin/gcc /usr/bin/g++ /usr/bin/cc
+	sudo ln -sf /usr/bin/gcc-7 /usr/bin/gcc
+	sudo ln -sf /usr/bin/g++-7 /usr/bin/g++
+	sudo ln -sf /usr/bin/gcc /usr/bin/cc
+	export PATH=/usr/lib/go-1.10/bin:$PATH
+	installDependency
+	configureAndInstall |& tee -a "$LOG_FILE"
+
+	;;
+
+"rhel-7.4" | "rhel-7.5" | "rhel-7.6")
 	printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
 	printf -- 'Installing the dependencies for Go from repository \n' |& tee -a "$LOG_FILE"
 	sudo yum install -y hostname git tar zip gcc-c++ unzip python libtool automake cmake curl wget gcc vim patch binutils-devel bzip2 make | tee -a "${LOG_FILE}"
-	buildGCC |& tee -a "$LOG_FILE"
+	buildGCC
 	buildGO |& tee -a "$LOG_FILE"
-	installDependency	
+	installDependency
 	configureAndInstall | tee -a "${LOG_FILE}"
 	;;
 
@@ -453,7 +467,7 @@ case "$DISTRO" in
 	installDependency
 	configureAndInstall | tee -a "${LOG_FILE}"
 	;;
-	
+
 "sles-15")
 	sudo zypper install -y wget git tar pkg-config zip unzip python libtool automake cmake zlib-devel gcc gcc-c++ binutils-devel patch which curl python-xml libxml2-devel ninja
 	buildGO |& tee -a "$LOG_FILE"
