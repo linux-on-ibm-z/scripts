b1fdf0359be3bc8d7fba2c6a6e1511e7ac0b1f5c
aborkar-ibm <aborkar@us.ibm.com>
2020-01-10 14:12:24 +0530

    Version 22.2

---
+++ Erlang/22.2/build_erlang.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+# © Copyright IBM Corporation 2020
+# LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+#
+# Instructions:
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/Erlang/22.2/build_erlang.sh
+# Execute build script: bash build_erlang.sh    (provide -h for help)
+
+set -e -o pipefail
+
+PACKAGE_NAME="erlang"
+PACKAGE_VERSION="22.2"
+CURDIR="$(pwd)"
+BUILD_DIR="/usr/local"
+
+TESTS="false"
+FORCE="false"
+LOG_FILE="$CURDIR/logs/${PACKAGE_NAME}-${PACKAGE_VERSION}-$(date +"%F-%T").log"
+
+trap cleanup 0 1 2 ERR
+
+#Check if directory exists
+if [ ! -d "$CURDIR/logs/" ]; then
+    mkdir -p "$CURDIR/logs/"
+fi
+
+# Need handling for RHEL 6.10 as it doesn't have os-release file
+if [ -f "/etc/os-release" ]; then
+    source "/etc/os-release"
+else
+    cat /etc/redhat-release >>"${LOG_FILE}"
+    export ID="rhel"
+    export VERSION_ID="6.x"
+    export PRETTY_NAME="Red Hat Enterprise Linux 6.x"
+fi
+
+function prepare() {
+    if command -v "sudo" >/dev/null; then
+        printf -- 'Sudo : Yes\n' >>"$LOG_FILE"
+    else
+        printf -- 'Sudo : No \n' >>"$LOG_FILE"
+        printf -- 'You can install the same from installing sudo from repository using apt, yum or zypper based on your distro. \n'
+        exit 1
+    fi
+
+    if [[ "$FORCE" == "true" ]]; then
+        printf -- 'Force attribute provided hence continuing with install without confirmation message\n' |& tee -a "$LOG_FILE"
+    else
+        # Ask user for prerequisite installation
+        printf -- "\nAs part of the installation , dependencies would be installed/upgraded.\n"
+        while true; do
+            read -r -p "Do you want to continue (y/n) ? :  " yn
+            case $yn in
+            [Yy]*)
+                printf -- 'User responded with Yes. \n' >>"$LOG_FILE"
+                break
+                ;;
+            [Nn]*) exit ;;
+            *) echo "Please provide confirmation to proceed." ;;
+            esac
+        done
+    fi
+}
+
+function cleanup() {
+    # Remove artifacts
+
+    if [ -f "$CURDIR/otp_src_${PACKAGE_VERSION}.tar.gz" ]; then
+        rm -rf "$CURDIR/otp_src_${PACKAGE_VERSION}.tar.gz"
+    fi
+    printf -- "Cleaned up the artifacts\n" >>"$LOG_FILE"
+}
+
+function configureAndInstall() {
+    printf -- "Configuration and Installation started \n"
+
+    # Download erlang
+    cd "$CURDIR"
+    wget "http://www.erlang.org/download/otp_src_${PACKAGE_VERSION}.tar.gz"
+    tar zxf otp_src_${PACKAGE_VERSION}.tar.gz
+    mv otp_src_${PACKAGE_VERSION} erlang
+    sudo chmod -Rf 755 erlang
+    sudo cp -Rf erlang "$BUILD_DIR"/erlang
+    printf -- "Download erlang success\n"
+
+    # Give permission to user
+    sudo chown -R "$USER" "$BUILD_DIR/erlang"
+
+    # Build and install erlang
+    cd "$BUILD_DIR"/erlang
+    export ERL_TOP=$(pwd)
+    ./configure
+    make
+    sudo make install
+    printf -- "Build and install erlang success\n" 
+
+    # Run Test
+    runTest
+
+    # Cleanup
+    cleanup
+
+    # Verify erlang installation
+    if command -v "erl" >/dev/null; then
+        printf -- " %s Installation verified.\n" "$PACKAGE_NAME"
+    else
+        printf -- "Error while installing %s, exiting with 127 \n" "$PACKAGE_NAME"
+        exit 127
+    fi
+}
+
+function runTest() {
+
+    set +e
+    if [[ "$TESTS" == "true" ]]; then
+        printf -- 'Running tests \n\n' |& tee -a "$LOG_FILE"
+        cd "$BUILD_DIR"/erlang
+        make release_tests
+        cd release/tests/test_server
+        $ERL_TOP/bin/erl -s ts install -s ts smoke_test batch -s init stop
+    fi
+
+    set -e
+}
+
+function logDetails() {
+    printf -- '**************************** SYSTEM DETAILS *************************************************************\n' >"$LOG_FILE"
+    if [ -f "/etc/os-release" ]; then
+        cat "/etc/os-release" >>"$LOG_FILE"
+    fi
+
+    cat /proc/version >>"$LOG_FILE"
+    printf -- '*********************************************************************************************************\n' >>"$LOG_FILE"
+    printf -- "Detected %s \n" "$PRETTY_NAME"
+    printf -- "Request details : PACKAGE NAME= %s , VERSION= %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" |& tee -a "$LOG_FILE"
+}
+
+# Print the usage message
+function printHelp() {
+    echo
+    echo "Usage: "
+    echo " build_erlang.sh  [-d debug] [-y install-without-confirmation] [-t install and run tests]"
+    echo
+}
+
+while getopts "h?dyt" opt; do
+    case "$opt" in
+    h | \?)
+        printHelp
+        exit 0
+        ;;
+    d)
+        set -x
+        ;;
+    y)
+        FORCE="true"
+        ;;
+    t)
+        TESTS="true"
+        ;;
+    esac
+done
+
+function gettingStarted() {
+    printf -- '\n********************************************************************************************************\n'
+    printf -- "\n*Getting Started * \n"
+    printf -- "Running erlang: \n"
+    printf -- "erl  \n\n"
+    printf -- "You have successfully started erlang.\n"
+    printf -- '**********************************************************************************************************\n'
+}
+
+logDetails
+prepare # Check Prerequisites
+DISTRO="$ID-$VERSION_ID"
+
+case "$DISTRO" in
+"ubuntu-16.04" | "ubuntu-18.04" | "ubuntu-19.10")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo apt-get update
+    sudo apt-get install -y autoconf fop flex gawk gcc g++ gzip libncurses-dev libssl-dev libxml2-utils make openjdk-8-jdk tar unixodbc-dev wget xsltproc |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"rhel-6.x")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo yum install -y autoconf gawk gcc gcc-c++ gzip java-1.8.0-ibm-devel libatomic_ops-devel libxml2-devel libxslt ncurses-devel openssl-devel make tar unixODBC-devel wget |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo yum install -y autoconf flex gawk gcc gcc-c++ gzip java-1.8.0-openjdk-devel libxml2-devel libxslt ncurses-devel openssl-devel make tar unixODBC-devel wget |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"sles-12.4")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo zypper install -y autoconf flex gawk gcc gcc-c++ gzip java-1_8_0-openjdk-devel libopenssl-devel libxml2-devel libxslt-tools ncurses-devel make tar unixODBC-devel wget xmlgraphics-fop |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"sles-15.1")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo zypper install -y autoconf flex gawk gcc gcc-c++ gzip java-1_8_0-openjdk-devel libopenssl-1_1-devel libxml2-devel libxslt-tools ncurses-devel make tar unixODBC-devel wget |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+*)
+    printf -- "%s not supported \n" "$DISTRO" |& tee -a "$LOG_FILE"
+    exit 1
+    ;;
+esac
+
+gettingStarted |& tee -a "$LOG_FILE"
