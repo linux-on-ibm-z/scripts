dfa7b06a83c25d52a05471713d68cdccbd47400c
aborkar-ibm <aborkar@us.ibm.com>
2020-02-11 10:47:38 +0530

    Removed RH6, SL15

--- Snappy-Java/1.1.7/build_snappyjava.sh
+++ Snappy-Java/1.1.7/build_snappyjava.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2019.
+# © Copyright IBM Corporation 2019, 2020.
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
@@ -22,15 +22,7 @@ if [ ! -d "$SOURCE_ROOT/logs/" ]; then
     mkdir -p "$SOURCE_ROOT/logs/"
 fi
 
-# Need handling for RHEL 6.10 as it doesn't have os-release file
-if [ -f "/etc/os-release" ]; then
-    source "/etc/os-release"
-else
-    cat /etc/redhat-release >>"${LOG_FILE}"
-    export ID="rhel"
-    export VERSION_ID="6.x"
-    export PRETTY_NAME="Red Hat Enterprise Linux 6.x"
-fi
+source "/etc/os-release"
 
 function prepare() {
     if command -v "sudo" >/dev/null; then
@@ -141,7 +133,7 @@ case "$DISTRO" in
     configureAndInstall |& tee -a "$LOG_FILE"
     export PATH=$JAVA_HOME/bin:$PATH
     ;;
-"rhel-6.x" | "rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0" )
+"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0" )
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
     printf -- "Installing dependencies... it may take some time.\n"
 	
@@ -166,7 +158,7 @@ case "$DISTRO" in
 	export PATH=$JAVA_HOME/bin:$PATH
 	configureAndInstall |& tee -a "$LOG_FILE"
     ;;
-"sles-12.4" | "sles-15" | "sles-15.1")
+"sles-12.4" | "sles-15.1")
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
     printf -- "Installing dependencies... it may take some time.\n"
     sudo zypper install -y automake autoconf libtool pkg-config gcc-c++ git-core wget tar make patch which curl net-tools cmake |& tee -a "$LOG_FILE"
