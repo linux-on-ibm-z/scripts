dd703597cfb86364322f76aa03f88d84b6412dc5
aborkar-ibm <aborkar@us.ibm.com>
2020-02-11 10:44:06 +0530

    Removed RH6, SL15, UB 19.04

--- etcd/3.4.3/build_etcd.sh
+++ etcd/3.4.3/build_etcd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2018, 2019.
+# © Copyright IBM Corporation 2018, 2020.
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
@@ -23,16 +23,7 @@ if [ ! -d "$CURDIR/logs/" ]; then
    mkdir -p "$CURDIR/logs/"
 fi
 
-# Need handling for RHEL 6.10 as it doesn't have os-release file
-if [ -f "/etc/os-release" ]; then
-    source "/etc/os-release"
-else
-    cat /etc/redhat-release >> "${LOG_FILE}"
-    export ID="rhel"
-    export VERSION_ID="6.x"
-    export PRETTY_NAME="Red Hat Enterprise Linux 6.x"
-fi
-
+source "/etc/os-release"
 
 function prepare() {
     if  command -v "sudo" > /dev/null ;
@@ -234,7 +225,7 @@ prepare #Check Prequisites
 DISTRO="$ID-$VERSION_ID"
 
 case "$DISTRO" in
-    "ubuntu-16.04" | "ubuntu-18.04" | "ubuntu-19.04" | "ubuntu-19.10")
+    "ubuntu-16.04" | "ubuntu-18.04" | "ubuntu-19.10")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         sudo apt-get update
@@ -247,7 +238,7 @@ case "$DISTRO" in
         sudo yum install -y curl git wget tar gcc which |& tee -a "${LOG_FILE}"
         configureAndInstall |& tee -a "${LOG_FILE}"
         ;;
-    "sles-12.4" | "sles-15" | "sles-15.1")
+    "sles-12.4" | "sles-15.1")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         sudo zypper install -y  curl git wget tar gcc which gzip|& tee -a "${LOG_FILE}"
