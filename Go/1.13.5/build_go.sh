934f213b2fcb1fe8f3378965c60dcb067a3dc440
aborkar-ibm <aborkar@us.ibm.com>
2020-02-13 12:46:04 +0530

    Added RH8.1

--- Go/1.13.5/build_go.sh
+++ Go/1.13.5/build_go.sh
@@ -171,7 +171,7 @@ case "$DISTRO" in
   configureAndInstall |& tee -a "${LOG_FILE}"
   ;;
 
-"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0")
+"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0" | "rhel-8.1")
 	printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
 	printf -- 'Installing the dependencies for Go from repository \n' |& tee -a "$LOG_FILE"
 	sudo yum install -y  tar wget gcc  |& tee -a "${LOG_FILE}"
