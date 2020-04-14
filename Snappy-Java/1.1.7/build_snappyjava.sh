088e568ea5f93b75e97280c1fb524c80a3625d02
aborkar-ibm <aborkar@us.ibm.com>
2020-04-14 12:13:15 +0530

    Updated distros

--- Snappy-Java/1.1.7/build_snappyjava.sh
+++ Snappy-Java/1.1.7/build_snappyjava.sh
@@ -133,14 +133,17 @@ case "$DISTRO" in
     configureAndInstall |& tee -a "$LOG_FILE"
     export PATH=$JAVA_HOME/bin:$PATH
     ;;
-"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0" )
+"rhel-7.6" | "rhel-7.7" | "rhel-7.8" | "rhel-8.0" | "rhel-8.1")
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
     printf -- "Installing dependencies... it may take some time.\n"
 	
 	if [[ "$DISTRO" == "rhel-8.0" ]]; then
 		sudo yum install -y automake which cmake autoconf libtool pkgconfig gcc-c++-8.2.1 libstdc++-static-8.2.1 git wget tar make patch curl java-1.8.0-openjdk java-1.8.0-openjdk-devel |& tee -a "$LOG_FILE"
-        export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
-    else
+                export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+	elif [[ "$DISTRO" == "rhel-8.1" ]]; then
+	        sudo yum install -y  automake which autoconf libtool pkgconfig gcc-c++ libstdc++-static git wget tar make patch curl java-1.8.0-ibm.s390x java-1.8.0-ibm-devel.s390x cmake |& tee -a "$LOG_FILE"
+		export JAVA_HOME=/etc/alternatives/java_sdk_ibm
+        else
 		sudo yum install -y  automake which autoconf libtool pkgconfig gcc-c++ libstdc++-static git wget tar make patch curl java-1.8.0-ibm.s390x java-1.8.0-ibm-devel.s390x |& tee -a "$LOG_FILE"
 		export JAVA_HOME=/etc/alternatives/java_sdk_ibm
 
@@ -158,7 +161,7 @@ case "$DISTRO" in
 	export PATH=$JAVA_HOME/bin:$PATH
 	configureAndInstall |& tee -a "$LOG_FILE"
     ;;
-"sles-12.4" | "sles-15.1")
+"sles-12.4" | "sles-12.5" | "sles-15.1")
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
     printf -- "Installing dependencies... it may take some time.\n"
     sudo zypper install -y automake autoconf libtool pkg-config gcc-c++ git-core wget tar make patch which curl net-tools cmake |& tee -a "$LOG_FILE"
