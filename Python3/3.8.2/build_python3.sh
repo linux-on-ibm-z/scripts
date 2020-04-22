53e1c718cc078dd7734641c2ebcfe065bff0f8ec
aborkar-ibm <aborkar@us.ibm.com>
2020-04-22 10:58:37 +0530

    Minor

--- Python3/3.8.2/build_python3.sh
+++ Python3/3.8.2/build_python3.sh
@@ -103,11 +103,9 @@ function runTest() {
                 if [[ $? != 0 ]]; then      
 
                         sed -n '/tests failed/,/tests skipped/p' test_results.log | sort | uniq >> tests_failed.log
-                        sed '/tests skipped/q' tests_failed.log >> rerun_tests.log 
-                        sed -i '$d' rerun_tests.log 
-
+                        sed -n '/test_/p' tests_failed.log >> rerun_tests.log 
+                        
                         cat > expected_failures.log << EOF
-
     test_generators test_multiprocessing_fork
     test_multiprocessing_forkserver test_multiprocessing_spawn
     test_pdb test_regrtest test_signal test_threading
