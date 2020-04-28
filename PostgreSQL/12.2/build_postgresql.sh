1906c19123c55b0a1ea0242d159642f4f8abc7c2
aborkar-ibm <aborkar@us.ibm.com>
2020-04-28 09:37:32 +0530

    updated distros

--- PostgreSQL/12.2/build_postgresql.sh
+++ PostgreSQL/12.2/build_postgresql.sh
@@ -160,7 +160,7 @@ case "$DISTRO" in
     configureAndInstall |& tee -a "$LOG_FILE"
 	;;
 
-"rhel-7.5" | "rhel-7.6" | "rhel-7.7" | "rhel-8.0" | "rhel-8.1")
+"rhel-7.6" | "rhel-7.7" | "rhel-7.8"| "rhel-8.0" | "rhel-8.1")
 	printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
 	printf -- 'Installing the dependencies for postgresql from repository \n' |& tee -a "$LOG_FILE"
 
@@ -173,7 +173,7 @@ case "$DISTRO" in
 	configureAndInstall |& tee -a "$LOG_FILE"
 	;;
 
-"sles-12.4" | "sles-15.1")
+"sles-12.4" | "sles-12.5" | "sles-15.1")
 	printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
 	printf -- 'Installing the dependencies for postgresql from repository \n' |& tee -a "$LOG_FILE"
 	sudo zypper install -y git gcc gcc-c++ make readline-devel zlib-devel bison flex gawk |& tee -a "$LOG_FILE"
