a47d55e51cec32d8420f3a364d62402017cd713a
aborkar-ibm <aborkar@us.ibm.com>
2020-08-11 17:37:54 +0530

    Removed SL 12.4

--- etcd/3.4.10/build_etcd.sh
+++ etcd/3.4.10/build_etcd.sh
@@ -254,7 +254,7 @@ case "$DISTRO" in
         sudo yum install -y curl git wget tar gcc which golang |& tee -a "${LOG_FILE}"
         configureAndInstall |& tee -a "${LOG_FILE}"
         ;;
-    "sles-12.4" | "sles-12.5" | "sles-15.1")
+    "sles-12.5" | "sles-15.1")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         sudo zypper install -y  curl git wget tar gcc which gzip |& tee -a "${LOG_FILE}"
