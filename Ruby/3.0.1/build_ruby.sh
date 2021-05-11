d7c17bd4b344232f9897ab9343f3c8d83bf5ad6b
rposts <rishi@ca.ibm.com>
2021-05-11 11:03:00 -0700

    Updated distros

--- Ruby/3.0.1/build_ruby.sh
+++ Ruby/3.0.1/build_ruby.sh
@@ -170,7 +170,7 @@ checkPrequisites  #Check Prequisites
 
 DISTRO="$ID-$VERSION_ID"
 case "$DISTRO" in
-"ubuntu-18.04" | "ubuntu-20.04" | "ubuntu-20.10")
+"ubuntu-18.04" | "ubuntu-20.04" | "ubuntu-21.04")
   printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
   sudo apt-get update > /dev/null
   sudo apt-get install -y gcc make wget tar bison flex openssl libssl-dev libdb-dev libgdbm-dev libreadline-dev |& tee -a "${LOG_FILE}"
