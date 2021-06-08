9eba1281fd721583be84954bd65d5ffcfcbd12e1
aborkar-ibm <aborkar@us.ibm.com>
2021-06-08 15:27:20 +0530

    Version 1.20.0

---
+++ NGINX/1.12.0/build_nginx.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# © Copyright IBM Corporation 2020, 2021.
+# LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+#
+# Instructions:
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/NGINX/1.20.0/build_nginx.sh
+# Execute build script: bash build_nginx.sh    (provide -h for help)
+
+set -e -o pipefail
+
+PACKAGE_NAME="nginx"
+PACKAGE_VERSION="1.20.0"
+CURDIR="$(pwd)"
+
+FORCE="false"
+LOG_FILE="$CURDIR/logs/${PACKAGE_NAME}-${PACKAGE_VERSION}-$(date +"%F-%T").log"
+
+trap cleanup 0 1 2 ERR
+
+#Check if directory exists
+if [ ! -d "$CURDIR/logs/" ]; then
+    mkdir -p "$CURDIR/logs/"
+fi
+
+source "/etc/os-release"
+
+function prepare() {
+    if command -v "sudo" >/dev/null; then
+        printf -- 'Sudo : Yes\n' >>"$LOG_FILE"
+    else
+        printf -- 'Sudo : No \n' >>"$LOG_FILE"
+        printf -- 'You can install the same from installing sudo from repository using apt, yum or zypper based on your distro. \n'
+        exit 1
+    fi
+
+    if [[ "$FORCE" == "true" ]]; then
+        printf -- 'Force attribute provided hence continuing with install without confirmation message\n' |& tee -a "$LOG_FILE"
+    else
+        # Ask user for prerequisite installation
+        printf -- "\nAs part of the installation , dependencies would be installed/upgraded.\n"
+        while true; do
+            read -r -p "Do you want to continue (y/n) ? :  " yn
+            case $yn in
+            [Yy]*)
+                printf -- 'User responded with Yes. \n' >>"$LOG_FILE"
+                break
+                ;;
+            [Nn]*) exit ;;
+            *) echo "Please provide confirmation to proceed." ;;
+            esac
+        done
+    fi
+}
+
+function cleanup() {
+    # Remove artifacts
+
+    if [ -f "$CURDIR/nginx-${PACKAGE_VERSION}.tar.gz" ]; then
+        rm -rf "$CURDIR/nginx-${PACKAGE_VERSION}.tar.gz"
+    fi
+
+    printf -- "Cleaned up the artifacts\n" >>"$LOG_FILE"
+}
+
+function configureAndInstall() {
+    printf -- "Configuration and Installation started \n"
+ 
+  # Download nginx
+  
+  
+    cd "$CURDIR"
+    wget http://nginx.org/download/nginx-${PACKAGE_VERSION}.tar.gz
+    tar xvf nginx-${PACKAGE_VERSION}.tar.gz
+    cd nginx-${NGINX_VERSION}
+    ./configure 
+     make
+    sudo make install
+
+    printf -- "Build nginx success\n"
+
+    # Create a symlink
+    sudo ln -sf /usr/local/nginx/sbin/nginx /usr/bin/nginx
+
+    printf -- "Installation nginx success\n"
+
+    # Verify nginx installation
+    if command -v "nginx" >/dev/null; then
+        printf -- " %s Installation verified.\n" "$PACKAGE_NAME"
+    else
+        printf -- "Error while installing %s, exiting with 127 \n" "$PACKAGE_NAME"
+        exit 127
+    fi
+}
+
+function logDetails() {
+    printf -- '**************************** SYSTEM DETAILS *************************************************************\n' >"$LOG_FILE"
+    if [ -f "/etc/os-release" ]; then
+        cat "/etc/os-release" >>"$LOG_FILE"
+    else
+        cat /etc/redhat-release >>"${LOG_FILE}"
+    fi
+
+    cat /proc/version >>"$LOG_FILE"
+    printf -- '*********************************************************************************************************\n' >>"$LOG_FILE"
+    printf -- "Detected %s \n" "$PRETTY_NAME"
+    printf -- "Request details : PACKAGE NAME= %s , VERSION= %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" |& tee -a "$LOG_FILE"
+}
+
+# Print the usage message
+function printHelp() {
+    echo
+    echo "Usage: "
+    echo " build_nginx.sh  [-d debug] [-y install-without-confirmation] [-t install and run tests]"
+    echo
+}
+
+while getopts "h?dy" opt; do
+    case "$opt" in
+    h | \?)
+        printHelp
+        exit 0
+        ;;
+    d)
+        set -x
+        ;;
+    y)
+        FORCE="true"
+        ;;
+    esac
+done
+
+function gettingStarted() {
+    printf -- '\n********************************************************************************************************\n'
+    printf -- "\n*Getting Started * \n"
+    printf -- "Running nginx: \n"
+    printf -- "nginx -c nginx.conf \n\n"
+    printf -- "You have successfully started nginx.\n"
+    printf -- '**********************************************************************************************************\n'
+}
+
+logDetails
+prepare #Check Prequisites
+DISTRO="$ID-$VERSION_ID"
+
+case "$DISTRO" in
+"ubuntu-18.04" | "ubuntu-20.04" | "ubuntu-21.04")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo apt-get update
+    sudo apt-get install -y wget tar gcc make libpcre3-dev openssl libssl-dev zlib1g zlib1g-dev |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"rhel-7.8" | "rhel-7.9" | "rhel-8.1" | "rhel-8.2" | "rhel-8.3")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo yum install -y pcre-devel wget tar xz gcc make zlib-devel diffutils |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+"sles-12.5" | "sles-15.2")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo zypper install -y pcre-devel wget tar xz gcc make zlib-devel diffutils gzip |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+
+*)
+    printf -- "%s not supported \n" "$DISTRO" |& tee -a "$LOG_FILE"
+    exit 1
+    ;;
+esac
+
+gettingStarted |& tee -a "$LOG_FILE"
