3e31f25059b26e026bc1517ed8740e596f760f09
aborkar-ibm <aborkar@us.ibm.com>
2021-07-19 09:49:27 +0530

    Update and rename NGINX/build_nginx.sh to NGINX/1.14.2/build_nginx.sh

--- NGINX/1.14.2/build_nginx.sh
+++ NGINX/1.14.2/build_nginx.sh
@@ -3,7 +3,7 @@
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
-# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/NGINX/build_nginx.sh
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/NGINX/1.14.2/build_nginx.sh
 # Execute build script: bash build_nginx.sh    (provide -h for help)
 
 set -e -o pipefail
