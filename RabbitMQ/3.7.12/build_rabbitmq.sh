68533eaf8f7495b319619c48084273bbcf497c25
aborkar-ibm <aborkar@us.ibm.com>
2021-07-20 11:06:44 +0530

    Update and rename RabbitMQ/build_rabbitmq.sh to RabbitMQ/3.7.12/build_rabbitmq.sh

--- RabbitMQ/3.7.12/build_rabbitmq.sh
+++ RabbitMQ/3.7.12/build_rabbitmq.sh
@@ -3,7 +3,7 @@
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
-# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/RabbitMQ/build_rabbitmq.sh
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/RabbitMQ/3.7.12/build_rabbitmq.sh
 # Execute build script: bash build_rabbitmq.sh    
 #
 
