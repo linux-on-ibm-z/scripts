e3b3dceea93ef5f280e165ae7cdd62b567cd4fdc
aborkar-ibm <aborkar@us.ibm.com>
2021-07-27 15:03:08 +0530

    Minor

--- Keystone/19.0.0/build_keystone.sh
+++ Keystone/19.0.0/build_keystone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2020, 2021.
+# © Copyright IBM Corporation 2021.
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
