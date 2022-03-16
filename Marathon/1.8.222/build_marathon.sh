e2a5417f31b488aaa770cc934d4dfcdb200f8ee6
aborkar-ibm <aborkar@us.ibm.com>
2022-03-16 16:23:00 +0530

    updated jdk version

--- Marathon/1.8.222/build_marathon.sh
+++ Marathon/1.8.222/build_marathon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2020, 2021.
+# © Copyright IBM Corporation 2020, 2022.
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
@@ -74,9 +74,9 @@ function configureAndInstall() {
 		if [[ "$JAVA_FLAV" == "ibmsdk" ]]  ;then
 			printf -- "Installing IBM SDK 8 for %s \n" "$DISTRO"
 			cd "$SOURCE_ROOT"
-			wget http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/8.0.6.36/linux/s390x/ibm-java-s390x-sdk-8.0-6.36.bin
+			wget http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/8.0.7.5/linux/s390x/ibm-java-s390x-sdk-8.0-7.5.bin
 			echo -en 'INSTALLER_UI=silent\nUSER_INSTALL_DIR=/opt/java-1.8.0-ibm\nLICENSE_ACCEPTED=TRUE' > installer.properties
-			sudo bash ibm-java-s390x-sdk-8.0-6.36.bin -i silent -f installer.properties || true
+			sudo bash ibm-java-s390x-sdk-8.0-7.5.bin -i silent -f installer.properties || true
 		fi
 	fi
 
