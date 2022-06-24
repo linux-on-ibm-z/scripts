23165ebd4eb36980958c7fac359cf7d488b8c541
vibhutisawant <41043754+vibhutisawant@users.noreply.github.com>
2022-06-24 20:53:53 +0530

    updated jdk version

--- ApacheMesos/1.11.0/build_mesos.sh
+++ ApacheMesos/1.11.0/build_mesos.sh
@@ -74,9 +74,9 @@ function configureAndInstall() {
     if [[ "$ID" == "ubuntu" && "$JAVA_FLAV" == "ibmsdk" ]]  ;then
     	printf -- "Installing IBM SDK 8 for Ubuntu \n"
         cd "$CURDIR"
-        wget http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/8.0.7.5/linux/s390x/ibm-java-s390x-sdk-8.0-7.5.bin
+	wget http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/8.0.7.6/linux/s390x/ibm-java-s390x-sdk-8.0-7.6.bin
         echo -en 'INSTALLER_UI=silent\nUSER_INSTALL_DIR=/opt/java-1.8.0-ibm\nLICENSE_ACCEPTED=TRUE' > installer.properties
-        sudo bash ibm-java-s390x-sdk-8.0-7.5.bin -i silent -f installer.properties
+        sudo bash ibm-java-s390x-sdk-8.0-7.6.bin -i silent -f installer.properties
 	export JAVA_HOME=/opt/java-1.8.0-ibm
     fi
     
