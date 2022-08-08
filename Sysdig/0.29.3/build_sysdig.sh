4c7d0af237b628d35cf432181c3508b242cb6346
aborkar-ibm <aborkar@us.ibm.com>
2022-08-08 14:57:54 +0530

    updated distros

--- Sysdig/0.29.3/build_sysdig.sh
+++ Sysdig/0.29.3/build_sysdig.sh
@@ -214,7 +214,7 @@ logDetails
 prepare
 DISTRO="$ID-$VERSION_ID"
 case "$DISTRO" in
-"ubuntu-18.04" | "ubuntu-20.04" | "ubuntu-21.10" | "ubuntu-22.04")
+"ubuntu-18.04" | "ubuntu-20.04" | "ubuntu-22.04")
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" | tee -a "$LOG_FILE"
     printf -- '\nInstalling dependencies \n' | tee -a "$LOG_FILE"
     sudo apt-get update >/dev/null
