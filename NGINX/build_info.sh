ab9b6e0798038f453fb0b4f90e974fe225f078d3
Sankalp <sankalpjha225@gmail.com>
2023-11-08 01:52:24 +0530

    Create build_info.sh

---
+++ NGINX/build_info.sh
@@ -0,0 +1,12 @@
+{
+    "maintainer": "Sankalp@ibm.com",
+    "package_name" : "nginx",
+    "github_url":"https://github.com/nginx/nginx",
+    "version": "1.24.0",
+    "default_branch" : "master",
+    "package_dir": "nginx/nginx",
+    "build_script": "1.24.0/build_nginx.sh",
+    "docker_build": false,
+    "validate_build_script": true,
+    "use_non_root_user": false
+}
