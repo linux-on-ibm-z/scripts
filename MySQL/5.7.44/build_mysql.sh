a81bffcf7cdde156b2cad3e7233e275748bcb847
linuxonz <Linux.On.Z.Open.Source.Eco@ibm.com>
2023-11-23 11:02:11 +0530

    Version 5.7.44

---
+++ MySQL/5.7.44/build_mysql.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+# © Copyright IBM Corporation 2023.
+# LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+#
+# Instructions:
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/MySQL/5.7.44/build_mysql.sh
+# Execute build script: bash build_mysql.sh    (provide -h for help)
+
+set -e -o pipefail
+
+PACKAGE_NAME="mysql-server"
+PACKAGE_VERSION="5.7.44"
+SOURCE_ROOT="$(pwd)"
+
+FORCE="false"
+LOG_FILE="$SOURCE_ROOT/logs/${PACKAGE_NAME}-${PACKAGE_VERSION}-$(date +"%F-%T").log"
+
+trap cleanup 0 1 2 ERR
+
+#Check if directory exists
+if [ ! -d "$SOURCE_ROOT/logs/" ]; then
+    mkdir -p "$SOURCE_ROOT/logs/"
+fi
+
+source "/etc/os-release"
+
+function prepare() {
+    if command -v "sudo" >/dev/null; then
+        printf -- 'Sudo : Yes\n' >>"$LOG_FILE"
+    else
+        printf -- 'Sudo : No \n' >>"$LOG_FILE"
+        printf -- 'You can install the same from installing sudo from repository using apt, yum or zypper based on your distro. \n' |& tee -a "$LOG_FILE"
+        exit 1
+    fi
+
+    if [[ "$FORCE" == "true" ]]; then
+        printf -- 'Force attribute provided hence continuing with install without confirmation message\n' |& tee -a "$LOG_FILE"
+    else
+        # Ask user for prerequisite installation
+        printf -- "\nAs part of the installation , dependencies would be installed/upgraded.\n" |& tee -a "$LOG_FILE"
+        while true; do
+            read -r -p "Do you want to continue (y/n) ? :  " yn
+            case $yn in
+            [Yy]*)
+                printf -- 'User responded with Yes. \n' >>"$LOG_FILE"
+                break
+                ;;
+            [Nn]*) exit ;;
+            *) echo "Please provide confirmation to proceed." ;;
+            esac
+        done
+    fi
+}
+
+function cleanup() {
+    # Remove artifacts
+	cd $SOURCE_ROOT
+	rm -rf mysql-server
+    printf -- "Cleaned up the artifacts\n" >>"$LOG_FILE"
+}
+
+function configureAndInstall() {
+    printf -- "Configuration and Installation started \n"
+
+	#Download the MySQL source code from Github
+	cd $SOURCE_ROOT
+	git clone https://github.com/mysql/mysql-server
+	cd mysql-server
+	git checkout mysql-${PACKAGE_VERSION}
+	mkdir build
+	cd build
+
+	#Configure MySQL
+	cmake .. -DDOWNLOAD_BOOST=1 -DWITH_BOOST=. -DWITH_SSL=system
+
+	make
+	sudo make install
+	printf -- "MySQL build completed successfully. \n"
+
+	# Run Tests
+    runTest
+}
+
+function runTest() {
+	set +e
+	if [[ "$TESTS" == "true" ]]; then
+		printf -- "TEST Flag is set, continue with running test \n"
+		cd $SOURCE_ROOT/mysql-server/build
+        make test
+        printf -- "Tests completed. \n"
+	fi
+	set -e
+}
+
+function logDetails() {
+    printf -- '**************************** SYSTEM DETAILS *************************************************************\n' >"$LOG_FILE"
+    if [ -f "/etc/os-release" ]; then
+        cat "/etc/os-release" >>"$LOG_FILE"
+    fi
+
+    cat /proc/version >>"$LOG_FILE"
+    printf -- '*********************************************************************************************************\n' >>"$LOG_FILE"
+    printf -- "Detected %s \n" "$PRETTY_NAME" >>"$LOG_FILE"
+    printf -- "Request details : PACKAGE NAME= %s , VERSION= %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" |& tee -a "$LOG_FILE"
+}
+
+# Print the usage message
+function printHelp() {
+    echo
+    echo "Usage: "
+    echo "bash build_mysql.sh  [-d debug] [-y install-without-confirmation] [-t install-with-tests] "
+    echo
+}
+
+while getopts "h?dyt" opt; do
+    case "$opt" in
+    h | \?)
+        printHelp
+        exit 0
+        ;;
+    d)
+        set -x
+        ;;
+    y)
+        FORCE="true"
+        ;;
+    t)
+        TESTS="true"
+	;;
+    esac
+done
+
+function gettingStarted() {
+    printf -- '\n********************************************************************************************************\n'
+    printf -- "                       Getting Started                \n"
+    printf -- " MySQL 5.x server installed successfully.       \n"
+    printf -- " Information regarding the post-installation steps can be found here : https://dev.mysql.com/doc/refman/5.7/en/postinstallation.html \n"
+    printf -- " Starting MySQL Server: \n"
+    printf -- " sudo useradd mysql   \n"
+    printf -- " sudo groupadd mysql \n"
+    printf -- " cd /usr/local/mysql  \n"
+    printf -- " sudo mkdir mysql-files \n"
+    printf -- " sudo chown mysql:mysql mysql-files \n"
+    printf -- " sudo chmod 750 mysql-files \n"
+    printf -- " sudo bin/mysqld --initialize --user=mysql \n"
+    printf -- " sudo bin/mysqld_safe --user=mysql & \n"
+    printf -- "           You have successfully started MySQL Server.\n"
+    printf -- '**********************************************************************************************************\n'
+}
+
+logDetails
+prepare #Check Prequisites
+DISTRO="$ID-$VERSION_ID"
+
+case "$DISTRO" in
+"rhel-7.8" | "rhel-7.9")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo yum install -y bison cmake gcc gcc-c++ git hostname make ncurses-devel openssl openssl-devel |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+
+"sles-12.5")
+    printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+    printf -- "Installing dependencies... it may take some time.\n"
+    sudo zypper install -y bison cmake gawk gcc gcc-c++ git-core libopenssl-devel make libyui-ncurses-devel |& tee -a "$LOG_FILE"
+    configureAndInstall |& tee -a "$LOG_FILE"
+    ;;
+
+*)
+    printf -- "%s not supported \n" "$DISTRO" |& tee -a "$LOG_FILE"
+    exit 1
+    ;;
+esac
+
+gettingStarted |& tee -a "$LOG_FILE"
