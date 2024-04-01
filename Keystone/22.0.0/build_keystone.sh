691b7745c5dafa5cb55d6dd60176d194aaada4c4
linuxonz <Linux.On.Z.Open.Source.Eco@ibm.com>
2024-04-01 08:46:30 +0530

    Fix minor

--- Keystone/22.0.0/build_keystone.sh
+++ Keystone/22.0.0/build_keystone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2022, 2023.
+# © Copyright IBM Corporation 2022, 2024.
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
@@ -340,8 +340,8 @@ case "$DISTRO" in
     sudo yum install -y python3-devel libffi-devel cargo curl wget openssl-devel gcc make gcc-c++ python3-mod_wsgi httpd httpd-devel procps sqlite-devel python3-pip perl
     
     echo "[mariadb]" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
-    echo "name = MariaDB-10.11.6" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
-    echo "baseurl=http://mirror.mariadb.org/mariadb-10.11.6/yum/rhel9-s390x" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
+    echo "name = MariaDB-10.11.7" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
+    echo "baseurl=http://mirror.mariadb.org/mariadb-10.11.7/yum/rhel9-s390x" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
     echo "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
     echo "gpgcheck=1" | sudo tee -a /etc/yum.repos.d/MariaDB.repo
 
