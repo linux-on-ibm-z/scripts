a258509896717ba51ec6eeb1a0dab835cbd5fab5
linuxonz <Linux.On.Z.Open.Source.Eco@ibm.com>
2024-07-09 16:12:42 +0530

    Fix broken

--- TensorflowTransform/1.14.0/build_tensorflow_transform.sh
+++ TensorflowTransform/1.14.0/build_tensorflow_transform.sh
@@ -156,7 +156,7 @@ function configureAndInstall() {
     printf -- 'Installing Apache Beam... \n'
 
     cd "${CURDIR}"                         
-    sudo GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=True pip3 install numpy==1.24.3 scipy==1.10.1 'apache-beam[gcp]'==2.47.0
+    sudo GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=True pip3 install numpy==1.24.3 scipy==1.14.0 'apache-beam[gcp]'==2.47.0
 
     printf -- 'Apache Beam installed successfully \n'
 
