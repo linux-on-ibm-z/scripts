d6a2c7d17f33339a92cfb444615cb33220b88bb4
linuxonz <Linux.On.Z.Open.Source.Eco@ibm.com>
2025-01-16 19:25:03 +0530

    trivy script update

--- script/scanner-scripts/trivy_image_scan.sh
+++ script/scanner-scripts/trivy_image_scan.sh
@@ -4,6 +4,28 @@ image_name=$IMAGE_NAME
 build_docker=$BUILD_DOCKER
 
 if [ $build_docker == true ];then
+	sudo yum install make wget -y
+	git clone https://github.com/aquasecurity/trivy-db.git
+	cd trivy-db
+	wget -q https://raw.githubusercontent.com/linux-on-ibm-z/scripts/refs/heads/master/script/trivy.patch
+	git apply trivy.patch
+ 
+  	GO_VERSION=1.23.1
+   	wget -q https://storage.googleapis.com/golang/go"${GO_VERSION}".linux-s390x.tar.gz
+	chmod ugo+r go"${GO_VERSION}".linux-s390x.tar.gz
+	sudo rm -rf /usr/local/go /usr/bin/go
+	sudo tar -C /usr/local -xzf go"${GO_VERSION}".linux-s390x.tar.gz
+	sudo ln -sf /usr/local/go/bin/go /usr/bin/
+	sudo ln -sf /usr/local/go/bin/gofmt /usr/bin/
+	sudo ln -sf /usr/bin/gcc /usr/bin/s390x-linux-gnu-gcc
+
+  	make db-fetch-langs db-fetch-vuln-list
+   	make build
+    	make db-build
+     	sudo cp /trivy-db/out/trivy.db /root/.cache/trivy/db/trivy.db
+        chmod 644 /root/.cache/trivy/db/trivy.db
+	export TRIVY_DB_FILE=./out/trivy.db
+ 
 	wget https://github.com/aquasecurity/trivy/releases/download/v0.45.0/trivy_0.45.0_Linux-S390X.tar.gz
 	tar -xf trivy_0.45.0_Linux-S390X.tar.gz
         chmod +x trivy
