c7eb5fedf82cb90b23bd648546a300c72fc96cd7
linuxonz <Linux.On.Z.Open.Source.Eco@ibm.com>
2025-12-31 13:22:24 +0530

    MySQl updated Distros

--- MySQL/8.4.7/build_mysql.sh
+++ MySQL/8.4.7/build_mysql.sh
@@ -247,7 +247,7 @@ case "$DISTRO" in
 
     configureAndInstall |& tee -a "$LOG_FILE"
     ;;
-"ubuntu-24.04" | "ubuntu-25.04" | "ubuntu-25.10")
+"ubuntu-24.04")
     printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
     printf -- "Installing dependencies... it may take some time.\n"
     sudo apt-get update
