1e802ddd1184de1dc4c0eff2c6e4c404a0c3cfa3
linuxonz <linux.on.z.open.source.eco@ibm.com>
2026-07-30 18:05:13 +0530

    Cloudstack updated distros

--- CloudStack/4.22.0.0/build_cloudstack.sh
+++ CloudStack/4.22.0.0/build_cloudstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © Copyright IBM Corporation 2024,2025
+# © Copyright IBM Corporation 2024,2026
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
@@ -100,10 +100,10 @@ function configureAndInstall() {
     curl -sSL $PATCH_URL/master.patch | git apply -
     cd packaging
 
-    search_line="cd ui && npm install && npm run build && cd .."
-    replacement_line='cd ui && sudo rm -rf /${SOURCE_ROOT}/cloudstack/dist/rpmbuild/BUILD/${PACKAGE_NAME}-${PACKAGE_VERSION}/ui/node_modules && npm install --force && NODE_OPTIONS="--max-old-space-size=4096" npm run build && cd ..'
-    replace_line_in_file centos8/cloud.spec "$search_line" "$replacement_line"
-
+    sed -i '/cd ui && npm install && npm run build && cd ../c\
+cd ui && sudo rm -rf ${SOURCE_ROOT}/cloudstack/dist/rpmbuild/BUILD/cloudstack-${PACKAGE_VERSION}/ui/node_modules && npm install --force && NODE_OPTIONS="--max-old-space-size=4096" npm run build && cd ..' \
+centos8/cloud.spec
+    
     # Build rpm
     ./package.sh -d centos8
 
@@ -135,14 +135,13 @@ function configureAndInstallUb() {
     git clone -b $PACKAGE_VERSION "https://github.com/apache/cloudstack.git"
     cd cloudstack
     curl -sSL $PATCH_URL/master.patch | git apply -
-    replacement_line="$(printf '\t')cd ui && npm install --force && NODE_OPTIONS=\"--max-old-space-size=4096\" npm run build && cd .."
-    search="Depends: \${misc:Depends}, \${python3:Depends}, genisoimage, nfs-common, python3-pip, python3-distutils \| python3-distutils-extra, python3-netaddr, uuid-runtime"
 
-    replace_line_in_file debian/control " nodejs (>= 12), lsb-release, dh-systemd | debhelper (>= 13)" " lsb-release, dh-systemd | debhelper (>= 13)"
-    replace_line_in_file debian/control " python (>= 2.7) | python2 (>= 2.7), python3 (>= 3), python-setuptools, python3-setuptools," " python3 (>= 3), python3-setuptools,"
-    replace_line_in_file debian/control "$search" "Depends: \${misc:Depends}, \${python3:Depends}, genisoimage, nfs-common, python3-netaddr"
-    replace_line_in_file debian/control "Depends: \${misc:Depends}, python3-pip, python3-dev, libffi-dev" "Depends: \${misc:Depends}, libffi-dev"
-    replace_line_in_file debian/rules "$(printf '\t')cd ui && npm install && npm run build && cd .." "$replacement_line"
+    sed -i 's# nodejs (>= 12), lsb-release, dh-systemd | debhelper (>= 13)# lsb-release, dh-systemd | debhelper (>= 13)#' debian/control
+    sed -i 's# python (>= 2.7) | python2 (>= 2.7), python3 (>= 3), python-setuptools, python3-setuptools,# python3 (>= 3), python3-setuptools,#' debian/control
+    sed -i 's#Depends: ${misc:Depends}, ${python3:Depends}, genisoimage, nfs-common, python3-pip, python3-distutils | python3-distutils-extra, python3-netaddr, uuid-runtime#Depends: ${misc:Depends}, ${python3:Depends}, genisoimage, nfs-common, python3-netaddr#' debian/control
+    sed -i 's#Depends: ${misc:Depends}, python3-pip, python3-dev, libffi-dev#Depends: ${misc:Depends}, libffi-dev#' debian/control
+    sed -i 's#^\tcd ui && npm install && npm run build && cd \.\.#\tcd ui \&\& npm install --force \&\& NODE_OPTIONS="--max-old-space-size=4096" npm run build \&\& cd ..#' debian/rules
+    
     if [[ $DISTRO == ubuntu-24.10 ]]; then
         sed -i '/mkdir -p $(DESTDIR)\/usr\/share\/$(PACKAGE)-marvin/s/$/ \&\& cp tools\/marvin\/dist\/marvin-\*.tar.gz tools\/marvin\/dist\/Marvin-\*.tar.gz/' debian/rules
     fi
@@ -170,46 +169,6 @@ function configureAndInstallUb() {
         exit 127
     fi
 }
-
-function configureAndInstallSLES() {
-    printf -- "Configuration and Installation started \n"
-
-    installLibvirt
-    # Build & Install CloudStack
-    printf -- 'Installing CloudStack\n'
-    cd $SOURCE_ROOT
-    rm -rf cloudstack
-    git clone -b $PACKAGE_VERSION "https://github.com/apache/cloudstack.git"
-    cd cloudstack
-    curl -sSL $PATCH_URL/master.patch | git apply -
-    cd packaging
-
-    search_line="cd ui && npm install && npm run build && cd .."
-    replacement_line='cd ui && sudo rm -rf /${SOURCE_ROOT}/cloudstack/dist/rpmbuild/BUILD/${PACKAGE_NAME}-${PACKAGE_VERSION}/ui/node_modules && npm install --force && NODE_OPTIONS="--max-old-space-size=4096" npm run build && cd ..'
-    replace_line_in_file centos8/cloud.spec "$search_line" "$replacement_line"
-    replace_line_in_file centos8/cloud.spec "BuildRequires: nodejs" "#BuildRequires: nodejs"
-
-    # Build rpm
-    ./package.sh -d centos8
-
-    # Install rpm
-    cd ../dist/rpmbuild/RPMS/noarch/
-    sudo rpm --nodeps -i cloudstack-common-${PACKAGE_VERSION}-1.noarch.rpm cloudstack-management-${PACKAGE_VERSION}-1.noarch.rpm cloudstack-ui-${PACKAGE_VERSION}-1.noarch.rpm cloudstack-baremetal-agent-${PACKAGE_VERSION}-1.noarch.rpm cloudstack-usage-${PACKAGE_VERSION}-1.noarch.rpm cloudstack-agent-${PACKAGE_VERSION}-1.noarch.rpm
-    printf -- "Build and install CloudStack success\n"
-
-    cd $SOURCE_ROOT
-    # Verify cloudstack installation
-    installed_version=$(rpm -qa cloudstack-agent*)
-    version=$(echo "$installed_version" | grep -oP '\d+\.\d+\.\d+\.\d+')
-    if [ "$version" == "$PACKAGE_VERSION" ]; then
-        printf -- " %s Installation verified.\n" "$PACKAGE_NAME"
-    else
-        printf -- "Error while installing %s, exiting with 127 \n" "$PACKAGE_NAME"
-        exit 127
-    fi
-}
-
-
 
 function logDetails() {
     printf -- '**************************** SYSTEM DETAILS *************************************************************\n' >"$LOG_FILE"
@@ -259,39 +218,14 @@ setupNodejs() {
     if [[ $DISTRO == ubuntu* ||  $DISTRO == sles* || $DISTRO == rhel-8.10 ]]; then
         nvm install 10
         nvm use 10
-
-        NVM_BIN=$(nvm which current | xargs dirname)
-        sudo ln -sf "$NVM_BIN/node" /usr/local/bin/node
-        sudo ln -sf "$NVM_BIN/npm" /usr/local/bin/npm
         printf -- "\n Installation of Node.js 10 was successful \n"
     else
         nvm install 14
         nvm alias default 14
         nvm use default
-
-        NVM_BIN=$(nvm which current | xargs dirname)
-        sudo ln -sf "$NVM_BIN/node" /usr/bin/node
-        sudo ln -sf "$NVM_BIN/npm" /usr/bin/npm
-        export PATH="$HOME/.nvm/versions/node/v14.*/bin:$PATH"
         printf -- "\n Installation of Node.js 14 was successful \n"
     fi
     node -v
-}
-
-replace_line_in_file() {
-    if [[ ! -f "$1" ]]; then
-        echo "Error: File '$1' not found!"
-        return 1
-    fi
-
-    sed -i.bak "/$2/c\\$3" "$1"
-
-    if [[ $? -eq 0 ]]; then
-        echo "Line replaced successfully in $1."
-        rm -f "$1.bak"
-    else
-        echo "Error: Failed to replace the line in $1."
-    fi
 }
 
 setupPython() {
@@ -301,10 +235,7 @@ setupPython() {
         sudo tar -xvf Python-${PYTHON2_VERSION}.tgz
         cd Python-${PYTHON2_VERSION} || exit
         sudo bash configure && sudo make && sudo make install
-        cd ..
-        sudo rm /usr/local/bin/python
-        sudo ln -s /usr/bin/python3 /usr/local/bin/python
-        sudo apt-get install -y python3-pip |& tee -a "$LOG_FILE"
+        sudo apt-get install -y python-is-python3 python3-pip |& tee -a "$LOG_FILE"
     elif [[ $DISTRO == rhel-10.* ]]; then                                                                                            
         wget https://www.python.org/ftp/python/${PYTHON3_VERSION}/Python-${PYTHON3_VERSION}.tgz                                       
         tar -xvzf Python-${PYTHON3_VERSION}.tgz                                                                                      
@@ -354,19 +285,19 @@ prepare # Check Prequisites
 
 case "$DISTRO" in
 
-    "rhel-8.10" | "rhel-9.4" | "rhel-9.6" | "rhel-10.0")
+    "rhel-8.10" | "rhel-9.6" | "rhel-9.7" | "rhel-10.0")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         if [[ $DISTRO != rhel-10.0 ]]; then
             sudo yum groupinstall -y "Development Tools" |& tee -a "$LOG_FILE"
         fi
-        sudo yum install -y --nobest git python3 python3-pip maven genisoimage createrepo nodejs npm \
+        sudo yum install -y git wget python3 python3-pip maven genisoimage createrepo nodejs npm \
         nfs-utils qemu-img ipmitool python3-devel python3-libvirt libvirt perl qemu-kvm rng-tools httpd \
         syslinux-tftpboot tftp-server libffi-devel ant curl chkconfig jq openssl-devel bzip2-devel zlib-devel |& tee -a "$LOG_FILE"
         if [[ $DISTRO == rhel-10.* ]]; then
-            sudo yum install -y --nobest mariadb mariadb-server jna rpm-build rpmdevtools
+            sudo yum install -y mariadb mariadb-server jna rpm-build rpmdevtools
         else
-            sudo yum install -y --nobest mysql mysql-server dhcp-server
+            sudo yum install -y mysql mysql-server dhcp-server
         fi
         setupJava 2>&1 | tee -a "$LOG_FILE"
         if [[ $DISTRO == rhel-8.* ]]; then
@@ -376,12 +307,12 @@ case "$DISTRO" in
             echo "$(mvn -v)"
         fi
         setupPython |& tee -a "$LOG_FILE"
-        setupNodejs 2>&1 | tee -a "$LOG_FILE"
+        setupNodejs
         sudo pip3 install mysql-connector-python
         configureAndInstall |& tee -a "$LOG_FILE"
         ;;  
 
-    "sles-15.6")
+    "sles-15.7")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         for pkg in sudo wget git java-17-openjdk java-17-openjdk-devel ant ant-junit python3-libvirt-python libvirt selinux-tools dhcp-server qemu-img qemu-kvm dhcp \
@@ -395,15 +326,15 @@ case "$DISTRO" in
         sudo mv apache-maven-3.8.8 /opt/maven
         echo -e "\nexport M2_HOME=/opt/maven\nexport MAVEN_HOME=/opt/maven\nexport PATH=\$PATH:\$M2_HOME/bin" | sudo tee -a /etc/environment > /dev/null
         source /etc/environment
-        setupNodejs |& tee -a "$LOG_FILE"
-        configureAndInstallSLES |& tee -a "$LOG_FILE"
+        setupNodejs
+        configureAndInstall |& tee -a "$LOG_FILE"
     ;;
 
    "ubuntu-22.04" | "ubuntu-24.04")
         printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
         printf -- "Installing dependencies... it may take some time.\n"
         sudo apt update -y
-        sudo apt-get install -y curl dpkg-dev debhelper openjdk-17-jdk genisoimage build-essential python3 python3-setuptools python3-mysql.connector \
+        sudo apt-get install -y curl git wget dpkg-dev debhelper openjdk-17-jdk genisoimage build-essential python3 python3-setuptools python3-mysql.connector \
         maven ant libjna-java nodejs npm mysql-client augeas-tools mysql-client qemu-utils rng-tools python3-dnspython qemu-kvm libvirt-daemon-system \
         ebtables vlan ipset python3-libvirt ethtool iptables cpu-checker libffi-dev rustc cargo jq |& tee -a "$LOG_FILE"
         export DEBIAN_FRONTEND=noninteractive
